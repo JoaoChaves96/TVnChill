76ba01778f8174b0a73dbfbe65d5260a9c49993e
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2018-05-18 18:15:53 +0100

    merge fix

--- App.js
+++ App.js
@@ -39,17 +39,6 @@ firebase.initializeApp({
   messagingSenderId: "69000225904"
 });
 
-// const Trakt = require('trakt.tv');
-
-// let options = {
-//   client_id: <the_client_id>,
-//   client_secret: <the_client_secret>,
-//   redirect_uri: null,   // defaults to 'urn:ietf:wg:oauth:2.0:oob'
-//   api_url: null,        // defaults to 'https://api.trakt.tv'
-//   useragent: null,      // defaults to 'trakt.tv/<version>'
-//   pagination: true      // defaults to false, global pagination (see below)
-// };
-// const trakt = new Trakt(options);
 
 
 
