08b92fcff7deb65aa8b447b163451eeec2060301
Unknown <inesferreira789@gmail.com>
2018-05-21 17:44:53 +0100

    Login page

--- screen/login.js
+++ screen/login.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { View, Button, Text, TouchableOpacity } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import {View, Button, Text, TouchableOpacity, Stylesheet, StyleSheet} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import {FormLabel, FormInput, FormValidationMessage} from 'react-native-elements';
 import * as firebase from 'firebase';
 
 export default class loginScreen extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { email: '', password: '', error: '', loading: false };
+        this.state = {email: '', password: '', error: '', loading: false};
     }
 
     onLoginPress() {
-        this.setState({ error: '', loading: true });
+        this.setState({error: '', loading: true});
 
-        const { email, password } = this.state;
+        const {email, password} = this.state;
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(() => {
                 console.log('Logged in successfully');
-                this.setState({ error: '', loading: false });
+                this.setState({error: '', loading: false});
                 this.props.navigation.navigate('LoggedIn');
             })
             .catch((error) => {
-                this.setState({ error: 'Authentication failed', loading: false });
+                this.setState({error: 'Authentication failed', loading: false});
                 console.log(error);
             })
     }
@@ -34,11 +34,17 @@ export default class loginScreen extends React.Component {
             <TouchableOpacity onPress={this.onLoginPress.bind(this)}>
                 <View
                     style={{
-                        width: '30%',
+                        width: 140,
+                        alignSelf: 'center',
                         borderRadius: 4,
-                        padding: 24,
+                        padding: 14,
+                        backgroundColor: '#119da4',
+                        marginTop: '10%',
                     }}>
-                    <Text style={{ textAlign: "center", color: '#3B5998', fontWeight: 'bold' }}>
+                    <Text style={{textAlign: 'center',
+                        color: 'white',
+                        fontWeight: 'bold',
+                        fontSize: 18}}>
                         Login
                     </Text>
                 </View>
@@ -48,16 +54,52 @@ export default class loginScreen extends React.Component {
 
     render() {
         return (
-            <View>
-                <FormLabel>Email</FormLabel>
-                <FormInput onChangeText={email => this.setState({ email })}
-                    placeholder='john@doe.com' />
-                <FormLabel>Password</FormLabel>
-                <FormInput onChangeText={password => this.setState({ password })}
-                    secureTextEntry
-                    placeholder='********' />
-                {this.renderButtonorLoading()}
+            <View style={styles.container}>
+                <View style={styles.titleView}>
+                    <Text style={styles.title}>Login</Text>
+                </View>
+                <View style={styles.form}>
+                    <Text style={styles.label}>Email</Text>
+                    <FormInput onChangeText={email => this.setState({email})}
+                               />
+                    <Text style={[styles.label, {marginTop:'6%'}]}>Password</Text>
+                    <FormInput onChangeText={password => this.setState({password})}
+                               secureTextEntry
+                               />
+                    {this.renderButtonorLoading()}
+                </View>
             </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#455561',
+    },
+    titleView : {
+        alignItems: 'center',
+        marginTop: '20%',
+    },
+    title : {
+        fontSize: 40,
+        color: "#119da4",
+        fontWeight: 'bold',
+        textShadowColor: 'rgba(0, 0, 0, 0.6)',
+        textShadowOffset: {width: 1, height: 1},
+        textShadowRadius: 1,
+    },
+    form : {
+        marginTop: '12%',
+    },
+    label : {
+        marginLeft: '8%',
+        fontSize: 17,
+        fontWeight: 'bold',
+        color:'#c6c7c4',
+    },
+});
+
+
