91b27c31b6db0944a671d218f8036535557cb492
Unknown <inesferreira789@gmail.com>
2018-05-27 17:33:56 +0100

    Clickable "Add to wishlist"

--- screen/MovieScreen.js
+++ screen/MovieScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View,Text,Image} from 'react-native';
+import {View,Text,Image, TouchableHighlight} from 'react-native';
 import{Icon, Container, Header, Content, Left, Body} from 'native-base';
+import firebase from 'firebase';
 
 export default class MovieScreen extends React.Component{
+
+    addtToWishlist = (id) => {
+        alert(id);
+    }
 
     render(){
         return (
@@ -35,10 +40,12 @@ export default class MovieScreen extends React.Component{
                                 <Icon name='star' style={{color:'white'}}/>
                                 <Text style={{marginLeft:'5%', marginTop:'3%', color:'white'}}>{this.props.navigation.state.params.movie.rating}</Text>
                             </View>
+                            <TouchableHighlight style={{marginBottom:'10%'}} onPress = {() => this.addtToWishlist(this.props.navigation.state.params.movie.id)}>
                             <View style={{height:25, marginTop:'8%', flexDirection:'row', flex:1}}>
                                 <Icon name='ios-desktop' style={{color:'white'}}/>
                                 <Text style={{marginLeft:'5%', marginTop:'3%', color:'white'}}>Add to Wishlist</Text>
                             </View>
+                            </TouchableHighlight>
                         </View>
                     </View>
                     <Text style={{marginTop:'3%', marginRight: '4%',color:'white', fontSize:13}}>{this.props.navigation.state.params.movie.overview}</Text>
